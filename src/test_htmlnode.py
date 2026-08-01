7c98a4427421ce17e934527c0d55741773f89146
Samuel .A <samuelashrafmakram1998@gmail.com>
2026-08-01 11:17:17 +0300

    feat: split delimiter

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -43,14 +43,12 @@ class TestHtmlNode(unittest.TestCase):
     def test_to_html_with_children(self):
         child_node = LeafNode("span", "child")
         parent_node = ParentNode("div", [child_node])
-        print(child_node, f"\n{parent_node}")
         self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
 
     def test_to_html_with_grandchildren(self):
         grandchild_node = LeafNode("b", "grandchild")
         child_node = ParentNode("span", [grandchild_node])
         parent_node = ParentNode("div", [child_node])
-        print(grandchild_node, f"\n{child_node}", f"\n{parent_node}")
         self.assertEqual(
             parent_node.to_html(),
             "<div><span><b>grandchild</b></span></div>",

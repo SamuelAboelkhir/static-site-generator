239b6c2302ee1de06ce6c3cb8446d0bf00f5302d
Samuel .A <samuelashrafmakram1998@gmail.com>
2026-08-14 11:22:41 +0300

    feat: splitting images and links

--- src/textnode.py
+++ src/textnode.py
@@ -63,6 +63,28 @@ def text_node_to_html_node(text_node: TextNode) -> LeafNode:
             )  # pyright: ignore[reportUnreachable]
 
 
+def extract_markdown_images(text: str) -> list[tuple[str, str]]:
+    images: list[str] = re.findall(r"\!\[.+?\)", text)
+    tuples: list[tuple[str, str]] = []
+    for image in images:
+        alt: list[str] = re.findall(r"\[(.*?)\]", image)
+        url: list[str] = re.findall(r"\((.*?)\)", image)
+        tuples.append((alt[0], url[0]))
+
+    return tuples
+
+
+def extract_markdown_links(text: str) -> list[tuple[str, str]]:
+    links: list[str] = re.findall(r"(?<!!)\[.+?\)", text)
+    tuples: list[tuple[str, str]] = []
+    for link in links:
+        alt: list[str] = re.findall(r"\[(.+?)\]", link)
+        url: list[str] = re.findall(r"\((.+?)\)", link)
+        tuples.append((alt[0], url[0]))
+
+    return tuples
+
+
 def split_nodes_delimiter(
     old_nodes: list[TextNode], delimiter: str, text_type: TextType
 ) -> list[TextNode]:
@@ -86,23 +108,55 @@ def split_nodes_delimiter(
     return new_nodes
 
 
-def extract_markdown_images(text: str) -> list[tuple[str, str]]:
-    images: list[str] = re.findall(r"\!\[.+?\)", text)
-    tuples: list[tuple[str, str]] = []
-    for image in images:
-        alt: list[str] = re.findall(r"\[(.*?)\]", image)
-        url: list[str] = re.findall(r"\((.*?)\)", image)
-        tuples.append((alt[0], url[0]))
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes: list[TextNode] = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(
+                TextNode(
+                    image[0],
+                    TextType.IMAGE,
+                    image[1],
+                )
+            )
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
 
-    return tuples
 
-
-def extract_markdown_links(text: str) -> list[tuple[str, str]]:
-    links: list[str] = re.findall(r"(?<!!)\[.+?\)", text)
-    tuples: list[tuple[str, str]] = []
-    for link in links:
-        alt: list[str] = re.findall(r"\[(.+?)\]", link)
-        url: list[str] = re.findall(r"\((.+?)\)", link)
-        tuples.append((alt[0], url[0]))
-
-    return tuples
+def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes: list[TextNode] = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+        for link in links:
+            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, link section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
